a917934e9d737eb710fc05648d300206f637ddbd
eduardo <eduardo.brandiao.adm@gmail.com>
2022-04-23 14:53:54 -0300

    component home done

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Navbar } from "../components/Navbar"
+
+import apiVehicles from '../utils/Api';
+
 export const Home = () => {
+  const [ cars, setCars] = useState([]);
 
+  const navigate = useNavigate();
 
+  const getAllVehicles = async () => {
+    try {
+      const vehicles = await apiVehicles.getCars();
+      setCars(cars);
+    } catch (error) {
+      console.error('na home!', error.status)
+      navigate('/login')
+    }
+  }
+
+  useEffect(() => {
+    getAllVehicles();
+  }, [])
 
     return (
         <div>
